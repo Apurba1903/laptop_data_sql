d32bcc6c463db1a83212d8462a8127c7e15d5d7e
Apurba1903 <apurba1903@gmail.com>
2025-06-10 09:58:34 +0600

    data cleaning done

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -198,52 +198,153 @@ SET cpu_name = REPLACE(REPLACE(Cpu, cpu_brand, ''), SUBSTRING_INDEX(REPLACE(Cpu,
 ALTER TABLE laptops
 DROP COLUMN Cpu;
 
+SELECT DISTINCT cpu_name
+FROM laptops;
+
+UPDATE laptops
+SET cpu_name = SUBSTRING_INDEX(TRIM(cpu_name), ' ', 2);
+
 SELECT * FROM laptops;
 
 
+# Fix ScreenResolution Column
+ALTER TABLE laptops
+ADD COLUMN resolution_width INTEGER AFTER ScreenResolution,
+ADD COLUMN resolution_height INTEGER AFTER resolution_width;
+
+UPDATE laptops
+SET resolution_width = SUBSTRING_INDEX(SUBSTRING_INDEX(ScreenResolution, ' ', -1), 'x', 1);
+
+UPDATE laptops
+SET resolution_height = SUBSTRING_INDEX(SUBSTRING_INDEX(ScreenResolution, ' ', -1), 'x', -1);
+
+ALTER TABLE laptops
+ADD COLUMN touchscreen INTEGER AFTER resolution_height;
+
+UPDATE laptops
+SET touchscreen = ScreenResolution LIKE '%Touch%';
+
+ALTER TABLE laptops
+ADD COLUMN ips INTEGER AFTER touchscreen;
+
+UPDATE laptops
+SET ips = ScreenResolution LIKE '%IPS%';
+
+ALTER TABLE laptops
+DROP COLUMN ScreenResolution;
+
+SELECT * FROM laptops;
+
+
+# Fix Memory Columns
+SELECT Memory
+FROM laptops;
+
+DELETE FROM laptops
+WHERE `index` = 713;
+
+ALTER TABLE laptops
+ADD COLUMN memory_type VARCHAR(255) AFTER Memory,
+ADD COLUMN primary_storage INTEGER AFTER memory_type,
+ADD COLUMN secondary_storage INTEGER AFTER primary_storage;
+
+UPDATE laptops
+SET Memory = NULL
+WHERE `index` = 713;
+
+SELECT DISTINCT Memory
+FROM laptops;
+
+SELECT Memory,
+CASE
+			WHEN Memory LIKE '%Flash Storage%' AND Memory LIKE '%HDD%' THEN 'Hybrid'
+			WHEN Memory LIKE '%SSD%' AND Memory LIKE '%HDD%' THEN 'Hybrid'
+			WHEN Memory LIKE '%Flash Storage%' THEN 'Flash Storage'
+            WHEN Memory LIKE '%Hybrid%' THEN 'Hybrid'
+            WHEN Memory LIKE '%HDD%' THEN 'HDD'
+            WHEN Memory LIKE '%SSD%' THEN 'SSD'            
+            ELSE NULL
+END AS 'memory_type'
+FROM laptops;
+
+UPDATE laptops
+SET memory_type = 	CASE
+												WHEN Memory LIKE '%Flash Storage%' AND Memory LIKE '%HDD%' THEN 'Hybrid'
+												WHEN Memory LIKE '%SSD%' AND Memory LIKE '%HDD%' THEN 'Hybrid'
+												WHEN Memory LIKE '%Flash Storage%' THEN 'Flash Storage'
+												WHEN Memory LIKE '%Hybrid%' THEN 'Hybrid'
+												WHEN Memory LIKE '%HDD%' THEN 'HDD'
+												WHEN Memory LIKE '%SSD%' THEN 'SSD'            
+												ELSE NULL
+									END;
+
+SELECT Memory,
+REGEXP_SUBSTR(SUBSTRING_INDEX(Memory, '+', 1), '[0-9]+' ),
+CASE
+		WHEN Memory LIKE '%+%' THEN REGEXP_SUBSTR(TRIM(SUBSTRING_INDEX(Memory, '+', -1)), '[0-9]+' )
+        ELSE 0
+END
+FROM laptops;
+
+UPDATE laptops
+SET primary_storage = 	REGEXP_SUBSTR(SUBSTRING_INDEX(Memory, '+', 1), '[0-9]+' );
+
+UPDATE laptops
+SET secondary_storage = 	CASE
+													WHEN Memory LIKE '%+%' THEN REGEXP_SUBSTR(TRIM(SUBSTRING_INDEX(Memory, '+', -1)), '[0-9]+' )
+													ELSE 0
+											END;
+
+SELECT primary_storage, secondary_storage,
+CASE WHEN primary_storage <= 2 THEN primary_storage*1024 ELSE primary_storage END,
+secondary_storage,
+CASE WHEN secondary_storage <= 2 THEN secondary_storage*1024 ELSE secondary_storage END
+FROM laptops;
+
+UPDATE laptops
+SET primary_storage = 	CASE WHEN primary_storage <= 2 THEN primary_storage*1024 ELSE primary_storage END;
+
+UPDATE laptops
+SET secondary_storage = 	CASE WHEN secondary_storage <= 2 THEN secondary_storage*1024 ELSE secondary_storage END;
+
+SELECT `index`, Memory, memory_type, primary_storage, secondary_storage
+FROM laptops;
+
+ALTER TABLE laptops
+DROP COLUMN Memory;
+
+SELECT * FROM laptops;
+
+
+# Removing gpu_name column
+ALTER TABLE laptops
+DROP COLUMN gpu_name;
+
+SELECT * FROM laptops;
+
+
+# Check Memory Consumption
+SELECT DATA_LENGTH/1024
+FROM information_schema.TABLES
+WHERE TABLE_SCHEMA = 'laptopdb' AND TABLE_NAME = 'laptops';
+
+
+# Check Distinct values
+SELECT DISTINCT Company FROM laptops;
+SELECT DISTINCT TypeName FROM laptops;
+SELECT DISTINCT Inches FROM laptops;
+SELECT DISTINCT resolution_width FROM laptops;
+SELECT DISTINCT resolution_height FROM laptops;
+SELECT DISTINCT cpu_brand FROM laptops;
+SELECT DISTINCT cpu_name FROM laptops;
+SELECT DISTINCT cpu_speed FROM laptops;
+SELECT DISTINCT Ram FROM laptops;
+SELECT DISTINCT memory_type FROM laptops;
+SELECT DISTINCT primary_storage FROM laptops;
+SELECT DISTINCT secondary_storage FROM laptops;
+SELECT DISTINCT gpu_brand FROM laptops;
+SELECT DISTINCT OpSys FROM laptops;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT * FROM laptops;
